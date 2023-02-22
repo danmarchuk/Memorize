8f56a765f9043cd568b9bfdfaaf716f85adf1fc6
Danyil Marchuk <zemero58@gmail.com>
2023-02-22 02:53:57 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Данік on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
