d553e96551ac6c304633f4fc016172d40cffc5a4
Danyil Marchuk <zemero58@gmail.com>
2023-03-02 17:36:46 +0100

    update the ViewModel code

--- Memorize/ViewModel/EmojiMemoryGame.swift
+++ Memorize/ViewModel/EmojiMemoryGame.swift
@@ -5,4 +5,18 @@
 //  Created by Данік on 01/03/2023.
 //
 
-import Foundation
+import SwiftUI
+
+// The ViewModes is an intermediary between the Model and the View
+class EmojiMemoryGame {
+    
+    // only the ViewModel's code itself can see the model
+    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 12)
+    
+    
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+}
