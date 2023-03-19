a3eccc6e0a51e1c66be0ef70c7101479fec2c795
Danyil Marchuk <zemero58@gmail.com>
2023-03-19 11:27:27 +0100

    commit

--- Memorize/ViewModel/EmojiMemoryGame.swift
+++ Memorize/ViewModel/EmojiMemoryGame.swift
@@ -10,8 +10,16 @@ import SwiftUI
 // The ViewModes is an intermediary between the Model and the View
 class EmojiMemoryGame {
     
+    func makeCardContent(index: Int) -> String {
+        return "🐵"
+    }
+    
     // only the ViewModel's code itself can see the model
-    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 12)
+    private var model: MemoryGame<String> =
+    MemoryGame<String>(numberOfPairsOfCards: 12) { index in
+        "🐵"
+        
+    }
     
     
     
