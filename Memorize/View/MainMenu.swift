5e9f76b4c7d14fd2da6fd06709ac96cee333d18f
Danyil Marchuk <zemero58@gmail.com>
2023-12-31 03:12:41 +0100

    add a possibility to choose from three games

---
+++ Memorize/View/MainMenu.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenu.swift
+//  Memorize
+//
+//  Created by Данік on 31/12/2023.
+//
+
+import SwiftUI
+
+struct MainMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenu()
+}
