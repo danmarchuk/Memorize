3ebfa572a6939378d440cdb37be0f529b09e8dd5
Danyil Marchuk <zemero58@gmail.com>
2024-01-02 18:32:40 +0100

    now the user can edit the name of the set, the color of the text and the emojis in it

---
+++ Memorize/View/CustomCellView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomCellView.swift
+//  Memorize
+//
+//  Created by Данік on 31/12/2023.
+//
+
+import Foundation
---
+++ Memorize/View/EditScreen.swift
@@ -0,0 +1,18 @@
+//
+//  EditScreen.swift
+//  Memorize
+//
+//  Created by Данік on 31/12/2023.
+//
+
+import SwiftUI
+
+struct EditScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditScreen()
+}
