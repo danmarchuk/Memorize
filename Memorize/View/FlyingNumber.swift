93cfd81fef7850af31cde22e201abc85fce6a91d
Danyil Marchuk <zemero58@gmail.com>
2024-01-05 20:11:45 +0100

    add a flying number

---
+++ Memorize/View/FlyingNumber.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumber.swift
+//  Memorize
+//
+//  Created by Данік on 05/01/2024.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumber()
+}
